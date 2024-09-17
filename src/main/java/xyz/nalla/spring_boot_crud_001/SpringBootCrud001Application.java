033feffb71de302cab84ddeb6269c9ba78c9ff1b
VENKY <venkynalla.az@outlook.com>
2024-09-17 09:08:55 -0400

    Removed comments.

--- src/main/java/xyz/nalla/spring_boot_crud_001/SpringBootCrud001Application.java
+++ src/main/java/xyz/nalla/spring_boot_crud_001/SpringBootCrud001Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootCrud001Application {
-	//Test Comment
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCrud001Application.class, args);
 	}
